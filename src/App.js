d1cb889bbe81e2793b1dc7d17d06c52a552550f3
Davi Gonçalves da Silva <davi-gsilva@outlook.com>
2026-01-03 18:21:28 -0300

    refactor: update board method to remove hardcode (AC #2)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>;
 }
-export function Board({ xIsNext, squares, currentMove, onPlay }) {
+export function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
       return;
@@ -16,28 +16,25 @@ export function Board({ xIsNext, squares, currentMove, onPlay }) {
   }
 
   const winner = calculateWinner(squares);
-  let status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const status = winner ? `Winner: ${winner}` : `Next player: ${xIsNext ? 'X' : 'O'}`;
+
+  const boardRows = [...Array(3)].map((_, row) => {
+    const squaresInRow = [...Array(3)].map((_, col) => {
+      const index = row * 3 + col;
+      return <Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />;
+    });
+
+    return (
+      <div key={row} className="board-row">
+        {squaresInRow}
+      </div>
+    );
+  });
 
   return (
     <>
-      <div className="status">
-        {status}
-      </div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-      </div>
+      <div className="status">{status}</div>
+      {boardRows}
     </>
   );
 }
@@ -81,7 +78,7 @@ export default function Game() {
   return (
     <div className="game">
       <div className="game-board">
-        <Board xIsNext={xIsNext} squares={currentSquares} currentMove={currentMove} onPlay={handlePlay} />
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
       <div className="game-info">
         <ol>
